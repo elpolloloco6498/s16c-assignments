bd11a0f55701a156a93735da10a4ac382b11a1f0
iletenoux <seedspace6498@gmail.com>
2024-01-04 09:32:07 +0400

    feat(test): fix timeout issue in test

--- Assignment2/tests/Assignment2ActionCode.test.js
+++ Assignment2/tests/Assignment2ActionCode.test.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+axios.defaults.timeout = 10000;
+const SECONDS = 1000;
+jest.setTimeout(70 * SECONDS)
 
 const webhook_url = 'https://eocowbx6y8mcm75.m.pipedream.net'
 const docsend_url = 'https://docsend.com/view/q5vyt5xb7yyeyvf2'
